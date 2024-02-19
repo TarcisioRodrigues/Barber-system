edc985203f6c58a5f5eb57a6a93b9fd3e6b06913
tarcisiorodrigues454@gmail.com <TarcisioRodrigues>
2024-02-19 10:18:53 -0300

    feat:format date

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,15 @@
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale"
 import { Header } from "../_components/header/header";
 
 export default function Home() {
   return (
-  <Header/>
+  <div>
+    <Header/>
+    <div className="px-5 pt-5">
+    <h2 className="text-xl font-bold">Olá Tarcisio </h2>
+    <p>{format(new Date(),"EEEE, d 'de' MMMM", { locale: ptBR} )}</p>
+    </div>
+  </div>
   );
 }
